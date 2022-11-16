49233787b3e3e58e592327ef16b7fb2403c77f53
TheW0k <95188434+TheW0k@users.noreply.github.com>
2022-11-16 17:48:40 +0100

    Initial commit

---
+++ PYTHON/16.11.22/01_PyPresentation.py
@@ -0,0 +1,30 @@
+"""
+# @Project: First Intruduction on Python
+# @author: TheW0k
+# @version: 1.0 2022-11-16
+
+"""
+#
+# Questo è un commento lineare in Python! Ciaooo
+# CRTL+L: Pulisce la Shell (console)
+# CRTL+N: Crea un nuovo foglio di lavoro/Progetto
+# Dark Theme: Ui theme: Clear dark
+#                         Text: Default dark
+# Convenzione Py:Nome file minuscolo, divisio con "_"
+#
+
+#------- Print -------
+print(456)
+print("Hello World!" )
+print("------------")
+
+#---------------------
+
+#------ Type --------
+print(type(123))    #int
+print(type(123.44)) #float
+print(type("Ciao")) #string
+
+
+
+
